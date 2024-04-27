54923839f496531809c0fe709fa7c31f474b2a7b
Noshadal <noshadali269@gmail.com>
2024-04-27 02:19:34 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function king(tag) {
+    console.log("me king ");
+    tag.src = "./king.jpg";
+    
+    // tag.alt = "king";
+}
+function queen(tag) {
+    console.log("me queen ");
+    tag.src = "./qeen.jpg";
+}
+function jack(tag) {
+    console.log("me jack ");
+    tag.src = "./jack.jpg";
+}
+function raja(tag) {
+    console.log("me raja ");
+    tag.src = "./703px-Card_back_01.svg.png";
+}
+function rani(tag) {
+    console.log("me rani ");
+    tag.src = "./703px-Card_back_01.svg.png";
+}
+function nukar(tag) {
+    console.log("me nukar ");
+    tag.src = "./703px-Card_back_01.svg.png";
+}
